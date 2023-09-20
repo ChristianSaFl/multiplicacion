b05506636ab5d69f7b59cd75c04db0e81a45f279
ChristianSaFl <88692429+ChristianSaFl@users.noreply.github.com>
2023-09-20 12:29:50 -0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+
+bitset<32> complemento_2(bitset<32> num)
+{
+	bool carry = 1;
+	for (int i = 0; i < num.size(); i++)
+	{
+		num[i] = !num[i]; if (carry) {
+			if (num[i] == 0) {
+				carry = 0;
+			}
+			num[i] = !num[i];
+		}
+	}
+	return num;
+}
+
+
+
+
+bitset<32> izquierda(bitset<32> num)
+{
+	bool sign = num[num.size() - 1]; num >>= 1;
+	num[num.size() - 1] = sign;
+
+	return num;
+}
+
+bitset<32> suma_b(bitset<32> a, bitset<32> b)
+{
+	bitset<32> res; bool carr = 0;
+	for (int i = 0; i < a.size(); i++)
+	{
+		bool sum = a[i] ^ b[i] ^ carr;
+		carr = (a[i] & b[i]) | (carr & (a[i] ^ b[i])); res[i] = sum;
+	}
+	return res;
+}
+
+
+
+
+int main()
+{
+	int n;
+	cout << "Ingrese la cantidad de bits (n): "; cin >> n;
+
+	bitset<32> multi, Q, q_1;
+	cout << "Ingrese el multiplicando: "; cin >> multi;
+	cout << "Ingrese el multiplicador: "; cin >> Q;
+
+
+	q_1[0] = Q[0];
+	for (int i = 1; i < n; i++)
+	{
+		q_1[i] = 0;
+	}
+
+	bitset<32> A;
+	for (int i = 0; i < n; i++)
+	{
+		if (Q[0] == 1 && q_1[0] == 0)
+		{
+			A = suma_b(A, multi);
+		}
+		else if (Q[0] == 0 && q_1[0] == 1)
+		{
+			A = suma_b(A, complemento_2(multi));
+		}
+
+		A = izquierda(A);
+
+		q_1 = Q;
+		Q >>= 1;
+	}
+
+	cout << "El resultado de la multiplicación en binario es: " << A << endl;
+	;
+
+	return 0;
+}
